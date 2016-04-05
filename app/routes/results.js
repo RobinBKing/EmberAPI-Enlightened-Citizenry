3180e2d095aa8253d4a5f500204b77451b133dae
Joey Arnstein and Rob King <joeyarnstein@gmail.com>
2016-04-05 09:34:43 -0700

    add initial files and lookup by zip code

---
+++ app/routes/results.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+
+export default Ember.Route.extend({
+ model: function(params) {
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=7d2b86cc56c54d03a4cd7012c406eab2&zip=' + params.zip;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     console.log(responseJSON.results)
+     return responseJSON.results;
+   });
+  }
+});
